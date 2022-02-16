8a7ee6ca4ce8f5aff0b885401c44edd30c2998ea
mychocopackagesATfreemailDOThu <mychocopackages@freemail.hu>
2022-02-16 04:17:05 +0100

    3.9

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/dvingerh/tb-vol-scroll/releases/download/3.9/tb-vol-scroll.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+  softwareName  = 'tb-vol-scroll*'
+  checksum      = '20D78C68CE76D774AF3D843EB727F8E161BD8BAE63822C6C3D280F0BD39CA4A8'
+  checksumType  = 'sha256'
+}
+
+$executable = "tb-vol-scroll.exe"
+
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath "$toolsDir\$executable" -Url $url
+
+# Move to startup folder
+$startuppath = [Environment]::GetFolderPath("Startup")
+$donloadpath = Join-Path $toolsDir $executable
+Move-Item $donloadpath $startuppath
+$fullpathtoexe = Join-Path $startuppath $executable
+& $fullpathtoexe
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,38 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'tb-vol-scroll*'
+  fileType      = 'exe'
+}
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | % {
+    $packageArgs['file'] = "$($_.UninstallString)" 
+    if ($packageArgs['fileType'] -eq 'MSI') {
+      $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
+      $packageArgs['file'] = ''
+    }
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
+}
+
+
+# Stop process before remove
+$psfile = Join-Path $env:TEMP stop-to-vol-scroll.ps1
+echo "Stop-Process -Name tb-vol-scroll" > $psfile
+Start-ChocolateyProcessAsAdmin "& `'$psfile`'"
+Remove-Item $psfile
+
+# Remove Program
+$executable = "tb-vol-scroll.exe"
+$startuppath = [Environment]::GetFolderPath("Startup")
+Remove-Item $startuppath/$executable
